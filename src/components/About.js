a4902f81da28b75ee89d66390fc4abef92669d3f
Moesha Keswani <keswanimoe@hotmail.co.uk>
2024-10-04 15:27:09 +0100

    About Me section updated

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,34 @@ export default function About() {
     return (
         <section id="about" style={{ padding: '30px'}}>
             <h2 style={{ padding: '40px'}}>About Me</h2>
+                <p>Hey! My name’s Moesha (in case that wasn't already obvious)</p>
                 <p>
-                I graduated from university in 2021 with a strong passion for coding, communicating, and problem-solving. 
-                Probably sounds super generic but it's the truth!
-                Throughout my academic and professional journey, I have gained proficiency in various programming languages and technologies; 
-                frequently attending tech events in London and engaging in personal programming projects, 
-                further expanding my knowledge and staying current with industry trends.
+                I'm an aspiring Software Engineer based in London, UK. I built this website because I didn’t have one and wanted a space that was more than just a CV but an extension of my personality!
                 </p>
+                <p>
+                Growing up, I was fascinated by technology, largely because I wanted to do what my older siblings were doing which was messing around with the family PC. The excitement of using a computer was undeniable, and every chance I got, I would dive into learning how to use it. Eventually, my family gifted me my own laptop, which only fuelled my passions. Since then, it’s safe to say I've become the go-to tech support for friends and family.
+                </p>
+                <p>
+                I've always been both creative and technical - probably the "Virgo" in me (a half-joke) - so when it came time to choose a university course, I was pretty confused. Should I pursue something creative or technical? Could I do both? I wasn't sure at the time, so I went for what seemed like the safer choice and chose to study Mathematics for its job security. However, as I started university, I was pretty nervous. Would I regret my decision? Would my creative side feel wasted? 
+                </p>
+                <p>
+                It definitely didn't take long for me to realise how wrong I was in trying to separate my creative and technical sides. It wasn’t until I started taking computing modules that I truly understood the value of my creative spark in solving complex problems to create algorithms. These modules challenged me to think creatively and approach problems from new angles and now my love for coding is endless. I compare it to playing Dr. Kawashima’s Brain Training—while coding might seem like a mundane task from the outside, it’s actually thrilling, mentally stimulating, and, at times, addictive. 
+                </p>
+                <p>
+                I believe the tech industry is where creativity and technical expertise can truly coexist, and I am sooo excited to work alongside like-minded individuals who are just as passionate about integrating creativity into technology to create efficient solutions as I am!
+                </p>
+
+            <h3 style={{ padding: '10px', textAlign: 'center'}}>Hobbies</h3>   
+            <h4 style={{ padding: '0 0 10px 0', textAlign: 'left'}}>Sewing</h4>   
+            <p>
+            As I became more aware of the environmental impact of fast fashion, it became clear that I could no longer ignore the issues. Fashion has always been a love of mine, but I hadn’t found the time to learn sewing until the pandemic gave me that opportunity. I now commit to exclusively buying second-hand clothes and fabrics, which I sometimes upcycle. I set myself a challenge to adopt a more sustainable lifestyle by reducing my consumption (stopping the purchase of new clothes), reusing (buying second-hand), and recycling (upcycling what I already own). Since learning, I have made so many garments and now consider myself a pretty great seamstress! You can see some of my sewing creations on <a href="https://www.instagram.com/moeshasewing/">Instagram</a> or <a href="https://www.tiktok.com/@moeshasewing?lang=en">TikTok</a>!
+            </p>
+            <h4 style={{ padding: '10px 0 10px 0', textAlign: 'left'}}>Travelling</h4>   
+            <p>I have a deep love for traveling and immersing myself in new cultures, especially through their food. Last year I visited Sicily, Albania, Switzerland, Amsterdam and this year, I spent five months backpacking across Asia. While every moment was incredible, it’s the food that always stands out in my memories (I still dream about the Khao Soi I had in Chiang Mai!). In the future I’d love to explore South America, but after this adventure, I’m taking a break because travel fatigue is very real!</p>
+            <h4 style={{ padding: '10px 0 10px 0', textAlign: 'left'}}>Games</h4>   
+            <p>
+            My brother is a huge gamer, I remember him competing in Street Fighter 6 tournaments when I was younger ordering crazy cool custom fighting sticks and I couldn't help admire the culture of it all. Right now, I love playing games on my Nintendo Switch - Some of my favourite games are Animal Crossing New Horizons, Cuphead and Inside but my family and I play a lot of Beach Buggy Racing, Mario Party and Overcooked (which can get very heated!). I am very tempted to start playing Astrobot and Stardew Valley because I've heard the best things about them both! 
+            </p>
         </section>
     )
 }
